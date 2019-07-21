163883f62e9ab6cba9cc3d90bd440ccfb84f4218
Fortes, Luis - Dell Team <L_Fortes@DellTeam.com>
2019-07-21 09:57:33 -0500

    adding lint task

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+const gulp = require('gulp');
+const jshint = require('gulp-jshint');
+
+gulp.task('lint', () => gulp.src(['./**/*.js', '!node_modules/**'])
+    .pipe(jshint({ esversion: 6 }))
+    .pipe(jshint.reporter('default'))
+    .pipe(jshint.reporter('fail'))
+);
+        
