08354b8d008ba42c345185c6829b7292d949b3d5
Fortes, Luis - Dell Team <L_Fortes@DellTeam.com>
2019-07-21 11:17:53 -0500

    fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const jshint = require('gulp-jshint');
 
 gulp.task('lint', () => gulp.src(['./**/*.js', '!node_modules/**'])
-    .pipe(jshint({ esversion: 6 }))
+//    .pipe(jshint({ esversion: 6 }))
     .pipe(jshint.reporter('default'))
     .pipe(jshint.reporter('fail'))
 );
